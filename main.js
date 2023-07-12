0ac032b7662523eac57af9a7b781b285a2b220d8
ayhous <ayouzi86@gmail.com>
2023-07-12 21:25:50 +0200

    changement

---
+++ main.js
@@ -0,0 +1,22 @@
+const click=document.querySelector(".globale");
+console.log(click)
+/* const click2=document.querySelector("#col2");
+const click3=document.querySelector("#col3");
+const click4=document.querySelector("#col4");
+const click5=document.querySelector("#col5");
+
+click1.addEventListener('click', changeSrc(this))
+click2.addEventListener('click', changeSrc(this))
+click3.addEventListener('click', changeSrc(this))
+click4.addEventListener('click', changeSrc(this))
+click5.addEventListener('click', changeSrc(this))
+ */
+function changeSrc(item) {
+   
+  
+   
+    item.hidden="true"
+  
+    console.log("je suis le pokemon 1");
+  }
+
